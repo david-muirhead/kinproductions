2b3cad914220c00193b00506a867308c5c396752
David Muirhead <davidmuirhead@Davids-MacBook-Pro.local>
2020-11-05 22:37:24 +1000

    updated purge

--- resources/index.blade.php
+++ resources/index.blade.php
@@ -27,8 +27,3 @@
     </div>
   @endwhile
 @endsection
-
-@section('svg')
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 356.6 356.6" aria-hidden="true" width="400" id="site-circle" class="z-20"><defs><radialGradient id="a" cx="178.3" cy="178.3" r="178.3" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#130c0e"/><stop offset="0" stop-color="#221716"/><stop offset="0" stop-color="#4a352c"/><stop offset="0" stop-color="#8a644f"/><stop offset="0" stop-color="#e2a57f"/><stop offset="0" stop-color="#f6b48a"/><stop offset=".76" stop-color="#f6b48a" stop-opacity=".3"/><stop offset=".76" stop-color="#f6b48a" stop-opacity=".29" id="myOffset"/><stop offset=".83" stop-color="#f6b48a" stop-opacity=".17"/><stop offset=".89" stop-color="#f6b48a" stop-opacity=".07"/><stop offset=".95" stop-color="#f6b48a" stop-opacity=".02"/><stop offset="1" stop-color="#f6b48a" stop-opacity="0"/></radialGradient></defs><path d="M178.3 356.6A178.3 178.3 0 100 178.3a178.3 178.3 0 00178.3 178.3" fill="url(#a)"/></svg> 
-@endsection
-
