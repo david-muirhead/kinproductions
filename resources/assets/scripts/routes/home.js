dd5669179b459720815113d16146b266f01d5d0a
David Muirhead <davidmuirhead@Davids-MacBook-Pro.local>
2020-11-06 13:47:07 +1000

    updated things

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -2,14 +2,14 @@ export default {
   init() {
     // JavaScript to be fired on the home page
 
-    // $( document ).ready(function() {
-    //   if (localStorage.getItem('pageloadcount')) { 
-    //     $('#aoc').hide();
-    //     $('#site-circle').hide();
-    //     $('.invisible').removeClass('invisible');
-    // } 
-    //   localStorage.setItem('pageloadcount', '1');
-    // });
+    $( document ).ready(function() {
+      if (localStorage.getItem('pageloadcount')) { 
+        $('#aoc').hide();
+        $('#site-circle').hide();
+        $('.invisible').removeClass('invisible');
+    } 
+      localStorage.setItem('pageloadcount', '1');
+    });
 
     $('#aoc').on('click', function() {     
        
